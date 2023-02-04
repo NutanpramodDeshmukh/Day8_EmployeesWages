9d91ea3552ebb93e76f35e0aeef34b11f418a4d8
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-04 12:24:11 +0530

    Started: Welcome to Employee wage computation program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage");
+            Console.WriteLine("Enter Employee Name: ");
+            string name = Console.ReadLine();
+            Random random = new Random();
+            int isPresent = random.Next(0, 2);
+
+            if (isPresent == 0)
+            {
+                Console.WriteLine(name + " is Absent");
+            }
+            else
+            {
+                Console.WriteLine(name + " is Present");
+            }
+        }
+    }
+}
